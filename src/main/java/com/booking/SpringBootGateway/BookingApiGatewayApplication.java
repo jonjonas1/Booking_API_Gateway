5d70d7196b4f0e366c35d3ababab10ee58782c73
Jon J <jj@>
2023-09-21 22:02:06 -0500

    SpringBoot Api Gateway

---
+++ src/main/java/com/booking/SpringBootGateway/BookingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.booking.SpringBootGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/SpringBootGateway/BookingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.SpringBootGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
